f2574a109e725aceefa273369ff2bfe8fcf45d87
dilltsao <dill0819@gmail.com>
2017-03-10 11:18:42 +0800

    實作記帳本程式

--- OnlineAccountBook/Controllers/HomeController.cs
+++ OnlineAccountBook/Controllers/HomeController.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineAccountBook.Models.ViewModels;
 
 namespace OnlineAccountBook.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            return View();
+        {            
+            List<AccountItemViewModels> models = new List<AccountItemViewModels>();
+
+            //模擬資料
+            models.Add(new AccountItemViewModels
+            {
+                Category = "支出",
+                Date = new DateTime(2017, 01, 01),
+                Money = 2000,
+                Description = "水電",
+            });
+
+            models.Add(new AccountItemViewModels
+            {
+                Category = "支出",
+                Date = new DateTime(2017, 02, 01),
+                Money = 2500,
+                Description = "水電",
+            });
+
+            models.Add(new AccountItemViewModels
+            {
+                Category = "支出",
+                Date = new DateTime(2017, 03, 01),
+                Money = 2200,
+                Description = "水電",
+            });
+
+            models.Add(new AccountItemViewModels
+            {
+                Category = "支出",
+                Date = new DateTime(2017, 04, 01),
+                Money = 15000,
+                Description = "水電",
+            });
+
+            return View(models);
         }
 
         public ActionResult About()
---
+++ OnlineAccountBook/Models/ViewModels/AccountItemViewModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineAccountBook.Models.ViewModels
+{
+    public class AccountItemViewModels
+    {
+        /// <summary>
+        /// 類別
+        /// </summary>
+        public string Category { get; set; }
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// 金額
+        /// </summary>
+        public double Money { get; set; }
+        /// <summary>
+        /// 備註
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
